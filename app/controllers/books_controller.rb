deba6baf4b4648365e6bc1e980f878043c54cce7
usagigensui <usagigensuii@gmail.com>
2021-05-13 13:16:07 +0000

    [Start] トップページと一覧ページの骨組みまで

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,26 @@
+class BooksController < ApplicationController
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :opinion)
+  end
+end
