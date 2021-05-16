58fa13f1913f92fd3a4426d0d400ea4ce69c47de
usagigensui <usagigensuii@gmail.com>
2021-05-16 03:35:44 +0000

    [Fix] 細かい修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,7 +24,12 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = current_user.books.find(params[:id])
+    @book = Book.find(params[:id])
+      if @book.user == current_user
+        render "edit"
+      else
+        redirect_to books_path
+      end
   end
 
   def update
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,12 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = current_user
+    @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def update
