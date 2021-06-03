a2a9f9f32f61d70f541ff5e54318b5cffb4573a5
usagigensui <usagigensuii@gmail.com>
2021-06-03 07:25:39 +0000

    [Add] 検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'search/search'
   get 'users/show'
   devise_for :users
   root to: "homes#top"
   get "home/about", to: "homes#about"
+  get "search" => "search#search"
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
